bb37dea44fc3690f958724ade72ad1cd236e41b3
ukml2018 <ukmlcal@gmail.com>
2024-05-27 16:19:34 +0530

    1st

--- application.py
+++ application.py
@@ -14,7 +14,7 @@ AZURE_ML_ENDPOINT = "https://aml-imperialbrand-ib.uksouth.inference.ml.azure.com
 AZURE_ML_KEY = "NtpIyqtbgohBK0CmbUfzsEhhbvzzJzLG"
 
 #Amju's code
-@app.route('/performance', methods=['GET'])
+@app.route('/performance/<store_id>', methods=['GET'])
 #def allowSelfSignedHttps(allowed):
     # bypass the server certificate verification on client side
 #    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
@@ -26,11 +26,13 @@ AZURE_ML_KEY = "NtpIyqtbgohBK0CmbUfzsEhhbvzzJzLG"
 # depending on the format your endpoint expects.
 # More information can be found here:
 # https://docs.microsoft.com/azure/machine-learning/how-to-deploy-advanced-entry-script
-def performance():
+def performance(store_id):
   #p=allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
   ssl._create_default_https_context = ssl._create_unverified_context
+  print(store_id)
   data = {
-    "outlet": "Esso Tankstelle outlet"
+    #"outlet": "Esso Tankstelle outlet"
+    "outlet":store_id
   }
 
   body = str.encode(json.dumps(data))
@@ -59,6 +61,40 @@ def performance():
       print(error.info())
       print(error.read().decode("utf8", 'ignore'))
       return jsonify({'error': 'Failed to get performance information from Azure ML endpoint'}), 500
+
+#Sasanka's code
+@app.route('/conversationalAI', methods=['POST'])
+def conversationalAI():
+  #p=allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+  ssl._create_default_https_context = ssl._create_unverified_context
+  data = { }
+
+  body = str.encode(json.dumps(data))
+
+  url = 'https://dbcmirepibmibgaipoc.uksouth.inference.ml.azure.com/score'
+  # Replace this with the primary/secondary key, AMLToken, or Microsoft Entra ID token for the endpoint
+  api_key = 'wdw3ssgTgianzh27TCZ7URMSsTDEreff'
+  if not api_key:
+      raise Exception("A key should be provided to invoke the endpoint")
+
+  # The azureml-model-deployment header will force the request to go to a specific deployment.
+  # Remove this header to have the request observe the endpoint traffic rules
+  headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'aml-imperialbrand-ib-4' }
+
+  req = urllib.request.Request(url, body, headers)
+
+  try:
+      response = urllib.request.urlopen(req)
+      result = response.read()
+      print(result)
+      return result
+      
+  except urllib.error.HTTPError as error:
+      print("The request failed with status code: " + str(error.code))
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+      print(error.info())
+      print(error.read().decode("utf8", 'ignore'))
+      return jsonify({'error': 'Failed to get ConversationalAL flow information from Azure ML endpoint'}), 500
 
 #@app.route('/performance/<outlet>', methods=['POST'])
 @app.route('/performance_old', methods=['POST'])
@@ -230,4 +266,4 @@ def reconnect():
         # Return an error message
         return jsonify({'error': 'Failed to get prediction from Azure ML endpoint'}), 500
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,port=8888)
