e9ac1ac738b4f169d9f37e762c7e0d4971909f98
ukml2018 <ukmlcal@gmail.com>
2024-05-28 08:45:47 +0530

    application.py changes

--- application.py
+++ application.py
@@ -13,7 +13,43 @@ app = Flask(__name__)
 AZURE_ML_ENDPOINT = "https://aml-imperialbrand-ib.uksouth.inference.ml.azure.com/score"
 AZURE_ML_KEY = "NtpIyqtbgohBK0CmbUfzsEhhbvzzJzLG"
 
-#Amju's code
+@app.route('/summary/<store_id>', methods=['GET'])
+def summary(store_id):
+  #p=allowSelfSignedHttps(True) # this line is needed if you use self-signed certificate in your scoring service.
+  ssl._create_default_https_context = ssl._create_unverified_context
+  print(store_id)
+  data = {
+    "category": "Summary",
+    "outlet":store_id
+  }
+
+  body = str.encode(json.dumps(data))
+
+  url = 'https://aml-imperialbrand-ib.uksouth.inference.ml.azure.com/score'
+  # Replace this with the primary/secondary key, AMLToken, or Microsoft Entra ID token for the endpoint
+  api_key = 'NtpIyqtbgohBK0CmbUfzsEhhbvzzJzLG'
+  if not api_key:
+      raise Exception("A key should be provided to invoke the endpoint")
+
+  # The azureml-model-deployment header will force the request to go to a specific deployment.
+  # Remove this header to have the request observe the endpoint traffic rules
+  headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'aml-imperialbrand-ib-4' }
+
+  req = urllib.request.Request(url, body, headers)
+
+  try:
+      response = urllib.request.urlopen(req)
+      result = response.read()
+      print(result)
+      return result
+      
+  except urllib.error.HTTPError as error:
+      print("The request failed with status code: " + str(error.code))
+    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+      print(error.info())
+      print(error.read().decode("utf8", 'ignore'))
+      return jsonify({'error': 'Failed to get Summary information from Azure ML endpoint'}), 500
+
 @app.route('/performance/<store_id>', methods=['GET'])
 #def allowSelfSignedHttps(allowed):
     # bypass the server certificate verification on client side
@@ -231,7 +267,7 @@ def reconnect(store_id):
   ssl._create_default_https_context = ssl._create_unverified_context
   print(store_id)
   data = {
-    "category": "reconnect",
+    "category": "Reconnect",
     "outlet":store_id
   }
 
