d5d40db8b3dd8f3d876ff0bba9000550b783ba3e
ukml2018 <ukmlcal@gmail.com>
2024-05-29 06:34:34 +0530

    application.py change

--- application.py
+++ application.py
@@ -86,17 +86,28 @@ def performance(store_id):
   req = urllib.request.Request(url, body, headers)
 
   try:
+      print("inside try")
       response = urllib.request.urlopen(req)
       result = response.read()
       print(result)
       return result
-      
+        
   except urllib.error.HTTPError as error:
-      print("The request failed with status code: " + str(error.code))
-    # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-      print(error.info())
-      print(error.read().decode("utf8", 'ignore'))
-      return jsonify({'error': 'Failed to get performance information from Azure ML endpoint'}), 500
+      try:
+        print("inside except try")
+        headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key), 'azureml-model-deployment': 'aml-imperialbrand-ib-4' }
+        req = urllib.request.Request(url, body, headers)  
+        response = urllib.request.urlopen(req)
+        result = response.read()
+        print(result)
+        return result
+      except urllib.error.HTTPError as error:
+        print("inside except")
+        print("The request failed with status code: " + str(error.code))
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+        print(error.read().decode("utf8", 'ignore'))
+        return jsonify({'error': 'Failed to get performance information from Azure ML endpoint'}), 500
 
 #Sasanka's code
 @app.route('/conversationalAI', methods=['GET'])
